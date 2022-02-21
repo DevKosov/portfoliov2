d63f302001e394b10d1cbae01ba2435e23a63a12
devkosov <devkosov@github.com>
2022-02-21 12:20:30 +0100

    first version

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-const imageFilters = document.querySelectorAll(".filter");
-
-imageFilters.forEach(image => {
-   image.addEventListener("mouseover", e => {image.children[0].children[0].firstElementChild.setAttribute("r","600");});
-   image.addEventListener("mouseout", e => {image.children[0].children[0].firstElementChild.setAttribute("r","0");});
-
-});
